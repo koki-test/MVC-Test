a97da516a7948f0f0dfa4a96765ef34ea19b85ea
MonoGitHub <rubencmrc@gmail.com>
2011-10-23 20:35:47 +0200

    commit burning test

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -28,6 +28,7 @@ namespace MvcTestApplication.Controllers
         {
             if (ModelState.IsValid)
             {
+                
                 if (Membership.ValidateUser(model.UserName, model.Password))
                 {
                     FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
